1a491a2fa8e34853fdb12de7854954cb436e705e
Marcel-aka-Satum <wilczynskimarceli@gmail.com>
2023-03-06 23:05:23 +0100

    Fixed connect backend with frontend

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -1,15 +1,30 @@
-import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Loginform from './components/Loginform';
 import Footer from './components/Footer/Footer'
 import {Routes, Route} from 'react-router-dom'
+import React, {useState, useEffect} from 'react'
 
 /////////////////// import css
 import "./components/Navbar/navbarStyle.css"
 import "./components/Footer/footerStyle.css"
 ///////////////////
 
+
 function App() {
+
+  //test if backend works
+  const [data, setData] = useState([{}])
+  useEffect(()=>{
+    fetch("/members").then(
+      res => res.json()
+    ).then(
+      data =>{
+        setData(data)
+        console.log(data)
+      }
+    )
+  }, []);
+
   return (
     <div id="main-container">
       <Navbar/>
