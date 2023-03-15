009dbed67e930fe3fd08de4459d71286eb7e35d2
Sunaj <nabil.elouaamari@student.uantwerpen.be>
2023-03-16 00:31:22 +0100

    Added some sample content for Rss, Article and Users

--- Frontend/my-app/src/components/Admin/Users.js
+++ Frontend/my-app/src/components/Admin/Users.js
@@ -6,34 +6,46 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default function Users() {
     const [users, setUsers] = useState([
-        {id: 1, name: 'John Doe', email: 'johndoe@example.com'},
-        {id: 2, name: 'Jane Doe', email: 'janedoe@example.com'},
-        {id: 3, name: 'Bob Smith', email: 'bobsmith@example.com'},
+        {id: 1, name: 'John Doe', email: 'johndoe@example.com', isAdmin: false},
+        {id: 2, name: 'Jane Doe', email: 'janedoe@example.com', isAdmin: false},
+        {id: 3, name: 'Bob Smith', email: 'bobsmith@example.com', isAdmin: false},
     ]);
     const [searchTerm, setSearchTerm] = useState('');
+    const [filter, setFilter] = useState('all');
 
     const handleSearchChange = (event) => {
         setSearchTerm(event.target.value);
     };
 
-    const filteredUsers = users.filter((user) =>
-        user.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
+    const filteredUsers = users.filter((user) => {
+        if (filter === 'admin') {
+            return user.isAdmin && user.name.toLowerCase().includes(searchTerm.toLowerCase());
+        } else {
+            return user.name.toLowerCase().includes(searchTerm.toLowerCase());
+        }
+    });
 
     const handleAddUser = (name, email) => {
-        const newUser = {id: users.length + 1, name, email};
+        const newUser = {id: users.length + 1, name, email, isAdmin: false};
         setUsers([...users, newUser]);
     };
 
-    const handleEditUser = (id, newName, newEmail) => {
+    const handleEditUser = (id, newName, newEmail, newIsAdmin) => {
         const updatedUsers = users.map((user) =>
-            user.id === id ? {id, name: newName, email: newEmail} : user
+            user.id === id ? {id, name: newName, email: newEmail, isAdmin: newIsAdmin} : user
         );
         setUsers(updatedUsers);
     };
 
     const handleDeleteUser = (id) => {
         const updatedUsers = users.filter((user) => user.id !== id);
+        setUsers(updatedUsers);
+    };
+
+    const handlePromoteUser = (id) => {
+        const updatedUsers = users.map((user) =>
+            user.id === id ? {...user, isAdmin: !user.isAdmin} : user
+        );
         setUsers(updatedUsers);
     };
 
@@ -52,16 +64,37 @@ export default function Users() {
                         />
                     </div>
                     <div>
+                        <div className="btn-group mb-3">
+                            <button
+                                type="button"
+                                className={`btn ${filter === 'all' ? 'btn-primary' : 'btn-outline-primary'}`}
+                                onClick={() => setFilter('all')}
+                            >
+                                All
+                            </button>
+                            <button
+                                type="button"
+                                className={`btn ${filter === 'admin' ? 'btn-primary' : 'btn-outline-primary'}`}
+                                onClick={() => setFilter('admin')}
+                            >
+                                Admin
+                            </button>
+                        </div>
                         <ul className="list-group">
                             {filteredUsers.map((user) => (
                                 <li className="list-group-item" key={user.id}>
                                     {user.name} ({user.email})
+                                    {user.isAdmin
+                                        ? <span className="badge bg-success ms-2">Admin</span>
+                                        : <span className="badge bg-primary ms-2">User</span>
+                                    }
                                     <div className="btn-group float-end">
                                         <button
                                             type="button"
+
                                             className="btn btn-primary"
                                             onClick={() =>
-                                                handleEditUser(user.id, prompt('Enter new name', user.name), prompt('Enter new email', user.email))
+                                                handleEditUser(user.id, prompt('Enter new name', user.name), prompt('Enter new email', user.email), user.isAdmin)
                                             }
                                         >
                                             Edit
@@ -73,6 +106,22 @@ export default function Users() {
                                         >
                                             Delete
                                         </button>
+                                        {!user.isAdmin
+                                            ? <button
+                                                type="button"
+                                                className="btn btn-success"
+                                                onClick={() => handlePromoteUser(user.id)}
+                                            >
+                                                Promote
+                                            </button>
+                                            : <button
+                                                type="button"
+                                                className="btn btn-warning"
+                                                onClick={() => handlePromoteUser(user.id)}
+                                            >
+                                                Demote
+                                            </button>
+                                        }
                                     </div>
                                 </li>
                             ))}
